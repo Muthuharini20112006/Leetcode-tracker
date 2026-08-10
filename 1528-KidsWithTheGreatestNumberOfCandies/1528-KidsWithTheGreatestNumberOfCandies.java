9c0fb9ca4a8186a5631554136cf811eeaadbd626
Muthuharini20112006 <harinimariappan8@gmail.com>
2026-08-10 15:50:48 +0530

    Create 1528-KidsWithTheGreatestNumberOfCandies

---
+++ 1528-KidsWithTheGreatestNumberOfCandies/1528-KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,22 @@
+// Last updated: 8/10/2026, 3:50:46 PM
+import java.util.*;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        int max = 0;
+
+        for(int c : candies) {
+            if(c > max)
+                max = c;
+        }
+
+        List<Boolean> ans = new ArrayList<>();
+
+        for(int c : candies) {
+            ans.add(c + extraCandies >= max);
+        }
+
+        return ans;
+    }
+}
