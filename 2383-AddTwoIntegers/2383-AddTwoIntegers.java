b8f34f3982e9e33f93c99b80a8018a114e57a542
Muthuharini20112006 <harinimariappan8@gmail.com>
2026-08-10 15:49:37 +0530

    Create 2383-AddTwoIntegers

---
+++ 2383-AddTwoIntegers/2383-AddTwoIntegers.java
@@ -0,0 +1,10 @@
+// Last updated: 8/10/2026, 3:49:36 PM
+class Solution {
+    public int sum(int num1, int num2) {
+        int sum;
+    sum=num1+num2;
+    return sum;   
+        
+        
+    }
+}
