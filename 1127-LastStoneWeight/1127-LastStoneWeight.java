3ab9a5fdf989dafc6f91cbbc2d0aa9efd157eabb
Muthuharini20112006 <harinimariappan8@gmail.com>
2026-08-10 15:52:06 +0530

    Create 1127-LastStoneWeight

---
+++ 1127-LastStoneWeight/1127-LastStoneWeight.java
@@ -0,0 +1,24 @@
+// Last updated: 8/10/2026, 3:52:05 PM
+import java.util.PriorityQueue;
+import java.util.Collections;
+
+class Solution {
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+        for (int stone : stones) {
+            maxHeap.offer(stone);
+        }
+
+        while (maxHeap.size() > 1) {
+            int first = maxHeap.poll();
+            int second = maxHeap.poll();
+
+            if (first != second) {
+                maxHeap.offer(first - second);
+            }
+        }
+
+        return maxHeap.isEmpty() ? 0 : maxHeap.poll();
+    }
+}
