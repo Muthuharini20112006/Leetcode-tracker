0262851b492e2cb93332b04d064c76c4c09f9759
Muthuharini20112006 <harinimariappan8@gmail.com>
2026-09-14 13:16:39 +0530

    Create 2600-KItemsWithTheMaximumSum

---
+++ 2600-KItemsWithTheMaximumSum/2600-KItemsWithTheMaximumSum.java
@@ -0,0 +1,23 @@
+// Last updated: 9/14/2026, 1:16:40 PM
+class Solution 
+{
+    public int kItemsWithMaximumSum(int numOnes, int numZeros, int numNegOnes, int k) {
+        int sum=0;
+        while(k>0){
+            if(numOnes>0){
+                sum+=1;
+                numOnes--;
+            }
+            else if(numZeros>0){
+                numZeros--;
+            }
+            else{
+                sum-=1;
+                numNegOnes--;
+            }
+            k--;
+            
+        }
+        return sum;
+    }
+}
