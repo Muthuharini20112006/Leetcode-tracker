927e559848129f27c1d9d98b334b1321dcf548c8
Muthuharini20112006 <harinimariappan8@gmail.com>
2026-09-14 13:25:37 +0530

    Create 175-CombineTwoTables

---
+++ 175-CombineTwoTables/175-CombineTwoTables.sql
@@ -0,0 +1,10 @@
+-- Last updated: 9/14/2026, 1:25:38 PM
+# Write your MySQL query statement below
+SELECT
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
