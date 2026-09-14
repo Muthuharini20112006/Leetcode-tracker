09e215aa6f38dea6bd29d6aa41ee34c2d95f824b
Muthuharini20112006 <harinimariappan8@gmail.com>
2026-09-14 13:20:17 +0530

    Create 844-BackspaceStringCompare

---
+++ 844-BackspaceStringCompare/844-BackspaceStringCompare.java
@@ -0,0 +1,23 @@
+// Last updated: 9/14/2026, 1:20:17 PM
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> stack1=new Stack<>();
+        Stack<Character> stack2=new Stack<>();
+        
+        for(char ch:s.toCharArray()){
+            if(ch!='#'){
+                stack1.push(ch);
+            }else if(!stack1.isEmpty())
+                stack1.pop();
+            }
+         
+        for(char ch:t.toCharArray()){
+            if(ch!='#'){
+                stack2.push(ch);
+            }else if(!stack2.isEmpty()){
+                stack2.pop();
+            }
+        }
+          return stack1.equals(stack2);
+    }
+}
