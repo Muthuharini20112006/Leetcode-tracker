a79eb28fc7618730176a02814e76f7bb2a8aefe9
Muthuharini20112006 <harinimariappan8@gmail.com>
2026-09-14 13:24:23 +0530

    Create 229-MajorityElementIi

---
+++ 229-MajorityElementIi/229-MajorityElementIi.java
@@ -0,0 +1,48 @@
+// Last updated: 9/14/2026, 1:24:24 PM
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int candidate1 = 0, candidate2 = 1;
+        int count1 = 0, count2 = 0;
+
+        // Find potential candidates
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Verify candidates
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == candidate1) {
+                count1++;
+            } else if (num == candidate2) {
+                count2++;
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+
+        if (count1 > nums.length / 3) {
+            result.add(candidate1);
+        }
+        if (count2 > nums.length / 3) {
+            result.add(candidate2);
+        }
+
+        return result;
+    }
+}
