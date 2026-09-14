6da1f67779961b3bf174d2b2761f1eff1a698127
Muthuharini20112006 <harinimariappan8@gmail.com>
2026-09-14 13:20:24 +0530

    Create 707-DesignLinkedList

---
+++ 707-DesignLinkedList/707-DesignLinkedList.java
@@ -0,0 +1,79 @@
+// Last updated: 9/14/2026, 1:20:25 PM
+class MyLinkedList {
+    class Node {
+        int val;
+        Node next;
+        Node(int val) {
+            this.val = val;
+        }
+    }
+    Node head;
+    public MyLinkedList() {
+
+    }
+    public int get(int index) {
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            if (temp == null) {
+                return -1;
+            }
+            temp = temp.next;
+        }
+        return temp == null ? -1 : temp.val;
+    }
+    public void addAtHead(int val) {
+        Node node = new Node(val);
+        node.next = head;
+        head = node;
+    }
+    public void addAtTail(int val) {
+        Node node = new Node(val);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = node;
+    }
+    public void addAtIndex(int index, int val) {
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            if (temp == null) {
+                return;
+            }
+            temp = temp.next;
+        }
+        if (temp == null) {
+            return;
+        }
+        Node node = new Node(val);
+        node.next = temp.next;
+        temp.next = node;
+    }
+    public void deleteAtIndex(int index) {
+        if (head == null) {
+            return;
+        }
+        if (index == 0) {
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; i < index - 1; i++) {
+            if (temp.next == null) {
+                return;
+            }
+            temp = temp.next;
+        }
+        if (temp.next != null) {
+            temp.next = temp.next.next;
+        }
+    }
+}
