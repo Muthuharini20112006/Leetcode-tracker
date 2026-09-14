fe0da47c1e70799185db695017db0bf0a473ee2e
Muthuharini20112006 <harinimariappan8@gmail.com>
2026-09-14 13:21:06 +0530

    Create 693-BinaryNumberWithAlternatingBits

---
+++ 693-BinaryNumberWithAlternatingBits/693-BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,20 @@
+// Last updated: 9/14/2026, 1:21:06 PM
+class Solution {
+    public boolean hasAlternatingBits(int n) {
+        int prev = n % 2;
+        n = n / 2;
+
+        while (n > 0) {
+            int curr = n % 2;
+
+            if (curr == prev) {
+                return false;
+            }
+
+            prev = curr;
+            n = n / 2;
+        }
+
+        return true;
+    }
+}
