ec7a7524ab4d2364dd4f47c2bdc77555491edc52
Muthuharini20112006 <harinimariappan8@gmail.com>
2026-09-14 13:23:48 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,10 @@
+// Last updated: 9/14/2026, 1:23:48 PM
+class Solution {
+    public boolean canWinNim(int n) {
+        if(n%4==0)
+        return false;
+        else 
+        return true;
+
+        }   
+    }
